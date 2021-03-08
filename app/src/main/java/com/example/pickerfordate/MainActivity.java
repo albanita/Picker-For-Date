b7287271c14fb775b122e69ad38a8e782273791c
albanita <43749327+albanita@users.noreply.github.com>
2021-03-09 00:45:21 +0100

    tarea 4.3.5 acabada

--- app/src/main/java/com/example/pickerfordate/MainActivity.java
+++ app/src/main/java/com/example/pickerfordate/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.pickerfordate;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showDatePicker(View view) {
+        DialogFragment df = new DatePickerFragment();
+        df.show(getSupportFragmentManager(), getString(R.string.datePicker));
+    }
+
+    public void processDatePickerResult(int year, int month, int day) {
+        String month_str = Integer.toString(month+1);
+        String day_str = Integer.toString(day);
+        String year_str = Integer.toString(year);
+        String dateMessage = (day_str + "/" + month_str + "/" + year_str);
+        Toast.makeText(this, getString(R.string.date_str) + dateMessage, Toast.LENGTH_SHORT).show();
     }
 }
